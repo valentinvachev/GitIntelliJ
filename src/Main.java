d961c928e709324e325ff82eda606ac66092cfc0
Valentin Vachev <v.vachevstz@gmail.com>
2022-02-19 21:28:22 +0200

    The test branch changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is first");
         System.out.println("This is first 2");
-        System.out.println("This is third part");
+        System.out.println("This is third part 22");
     }
 }
