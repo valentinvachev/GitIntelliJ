87c1417006c28810e1d393cc78095738d633280e
Valentin Vachev <v.vachevstz@gmail.com>
2022-02-20 12:57:02 +0200

    This is fourth part

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("This is first 2");
         System.out.println("This is third part");
 
-        System.out.println("Thiird partttt");
+        System.out.println("Fourth part");
     }
 }
