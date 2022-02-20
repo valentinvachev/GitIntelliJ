f30e06803accbcd39edbc9bf8060619425c6a5d5
Valentin Vachev <60047503+valentinvachev@users.noreply.github.com>
2022-02-20 13:27:57 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
         System.out.println("Thiird partttt");
           System.out.println("Thiird another part");
+          System.out.println("Thiird another part 3");
     }
 }
